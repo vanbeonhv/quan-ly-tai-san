dd6c22109ad5569d3dc4c221c8b59abcad5f85cc
marc <nguyenvannhv26@gmail.com>
2025-08-13 00:14:16 +0700

    init

---
+++ src/Model/TaiSan.java
@@ -0,0 +1,61 @@
+package Model;
+
+public class TaiSan {
+    private String maTaiSan;
+    private String tenTaiSan;
+    private String loaiTaiSan;
+    private String viTriPhong;
+    private double giaTri;
+
+    public TaiSan() {
+    }
+
+    public TaiSan(String maTaiSan, String tenTaiSan, String loaiTaiSan, String viTriPhong, double giaTri) {
+        this.maTaiSan = maTaiSan;
+        this.tenTaiSan = tenTaiSan;
+        this.loaiTaiSan = loaiTaiSan;
+        this.viTriPhong = viTriPhong;
+        this.giaTri = giaTri;
+    }
+
+    public String getMaTaiSan() {
+        return maTaiSan;
+    }
+
+    public String getTenTaiSan() {
+        return tenTaiSan;
+    }
+
+    public String getLoaiTaiSan() {
+        return loaiTaiSan;
+    }
+
+    public String getViTriPhong() {
+        return viTriPhong;
+    }
+
+    public double getGiaTri() {
+        return giaTri;
+    }
+
+    public void setMaTaiSan(String maTaiSan) {
+        this.maTaiSan = maTaiSan;
+    }
+
+    public void setTenTaiSan(String tenTaiSan) {
+        this.tenTaiSan = tenTaiSan;
+    }
+
+    public void setLoaiTaiSan(String loaiTaiSan) {
+        this.loaiTaiSan = loaiTaiSan;
+    }
+
+    public void setViTriPhong(String viTriPhong) {
+        this.viTriPhong = viTriPhong;
+    }
+
+    public void setGiaTri(double giaTri) {
+        this.giaTri = giaTri;
+    }
+    
+}
---
+++ src/quan/ly/tai/san/QuanLyTaiSan.java
@@ -0,0 +1,13 @@
+package quan.ly.tai.san;
+
+import View.JFTaiSan;
+
+public class QuanLyTaiSan {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("quan.ly.tai.san.QuanLyTaiSan.main()");
+        JFTaiSan view = new JFTaiSan();
+        view.show();
+    }
+    
+}
